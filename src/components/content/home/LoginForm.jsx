2d2b0c0f3681608d9258cdff11a5022f7ddee9f1
hadiasta <hijrihadi@gmail.com>
2025-12-16 18:49:07 +0700

    feat : finish logic validator login page feat : animation for login page
    Note: need to hash inputed password user for payload login

--- src/components/content/home/LoginForm.jsx
+++ src/components/content/home/LoginForm.jsx
@@ -1,9 +1,10 @@
 'use client'
-import { useState, useReducer } from 'react'
+import { useState, useReducer, useEffectEvent } from 'react'
 import StyledInput from '@/components/form/StyledInput'
 import ButtonStyled from '@/components/global/ButtonStyled'
 import { toast } from 'sonner'
-import { emailHelper } from '@/lib/helper'
+import { emailHelper, minLength, checkEmpty } from '@/lib/helper'
+import { cn } from '@/lib/utils'
 
 // form login admin Email & Password
 
@@ -11,24 +12,76 @@ const LoginForm = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleReducer = (state, action) => {
+  // initial state useReducer
+
+  // function useReducer untuk validator password dan email
+  const inputValidator = (state, action) => {
     switch (action.type) {
+      case 'email_required': {
+        return {
+          // update state lama + state baru
+          ...state,
+          emailRequired: action.nextEmailRequired,
+        }
+      }
       case 'email_checker': {
         return {
+          // update state lama + state baru
+          ...state,
           email: action.nextEmail,
         }
       }
-      case 'changed_name': {
+      case 'passsword_required': {
         return {
-          name: action.nextName,
-          age: state.age,
+          ...state,
+
+          passwordRequired: action.nextPasswordRequired,
+        }
+      }
+      case 'password_checker': {
+        return {
+          ...state,
+          password: action.nextPassword,
         }
       }
     }
     throw Error('Unknown action.')
   }
 
-  const [state, dispatch] = useReducer(handleReducer, { email: false, password: false })
+  const [state, dispatch] = useReducer(inputValidator, {
+    emailRequired: false,
+    email: false,
+    passwordRequired: false,
+    password: false,
+  })
+
+  //check email and password kosong  trigger setiap onchange input
+  const onInputedValue = useEffectEvent((value, field) => {
+    // check email empty string ?
+    switch (field) {
+      case 'email':
+        const isEmailEmpty = checkEmpty(value)
+        if (isEmailEmpty) {
+          dispatch({ type: 'email_required', nextEmailRequired: true })
+        } else {
+          dispatch({ type: 'email_required', nextEmailRequired: false })
+        }
+
+        break
+      case 'password':
+        // check password empty string ?
+        const isPasswordEmpty = checkEmpty(value)
+        if (isPasswordEmpty) {
+          dispatch({ type: 'passsword_required', nextPasswordRequired: true })
+        } else {
+          dispatch({ type: 'passsword_required', nextPasswordRequired: false })
+        }
+        break
+
+      default:
+        break
+    }
+  })
 
   const handleInput = (e, key) => {
     switch (key) {
@@ -42,13 +95,25 @@ const LoginForm = () => {
           // kalau salah format munculkan warning
           dispatch({ type: 'email_checker', nextEmail: true })
         }
+        // useEffectEvent check email empty string ?
+        onInputedValue(e, 'email')
+        // set email input terupdate
         setEmail(e)
 
-        // console.log(emailChecker)
         break
       case 'password':
+        // check password minimum 8 characther
+        // value, minimum length
+        const checkPasswordLength = minLength(e, 8)
+        if (checkPasswordLength) {
+          dispatch({ type: 'password_checker', nextPassword: false })
+        } else {
+          dispatch({ type: 'password_checker', nextPassword: true })
+        }
+
+        onInputedValue(e, 'password')
         setPassword(e)
-        // console.log(e, 'password')
+
         break
 
       default:
@@ -60,10 +125,12 @@ const LoginForm = () => {
     switch (key) {
       case 'email':
         return 'Please Input Email Properly e.g. user@gmail.com'
-
+      case 'email_required':
+        return 'Email Cannot be Empty !!!'
       case 'password':
-        return 'Please Input Email Properly e.g. user@gmail.com'
-      // break;
+        return 'Minimum 8 Characther, Please re-Input Your Password'
+      case 'password_required':
+        return 'Password Cannot be Empty !!!'
 
       default:
         break
@@ -77,6 +144,8 @@ const LoginForm = () => {
 
   return (
     <>
+      {/* ○ Required
+        ○ Must be email format */}
       <h3 className="input_label">{`Email`}</h3>
       <StyledInput
         className="mt-3"
@@ -85,10 +154,13 @@ const LoginForm = () => {
         type="text"
         value={email}
       />
-
-      <div className="mt-3 text-red-600">{state.email && getErrorMessage('email')}</div>
- 
-
+      {/* key agar retrigger css animation saat re-render  */}
+      <div key={`${state.emailRequired}-${state.email}`} className="warning_label  animate-fade-in">
+        <div className="mt-2 ">{state.emailRequired && getErrorMessage('email_required')}</div>
+        <div className="mt-2 ">{state.email && getErrorMessage('email')}</div>
+      </div>
+      {/* ○ Required
+        ○ Minimal character 8 */}
       <h3 className="input_label mt-3">{`Password`}</h3>
       <StyledInput
         className="mt-3"
@@ -97,7 +169,11 @@ const LoginForm = () => {
         type="password"
         value={password}
       />
-
+      
+      <div key={`${state.passwordRequired}-${state.password}`}  className="warning_label animate-fade-in">
+        <div className="mt-2">{state.passwordRequired && getErrorMessage('password_required')}</div>
+        <div className="mt-2">{state.password && getErrorMessage('password')}</div>
+      </div>
       <ButtonStyled
         className="mt-5"
         label="Masuk"
--- src/lib/helper.js
+++ src/lib/helper.js
@@ -2,3 +2,13 @@ export function emailHelper(email) {
 const detailedRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return detailedRegex.test(email);
 }
+
+
+export const minLength = (value = "", length = 8) => {
+  return value.trim().length >= length
+}
+
+
+export const checkEmpty = (value) => {
+  return value.trim() ===  ""
+}
